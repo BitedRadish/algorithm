7611208d8584e3aa8998ef5bf5c447af38dd98e9
BitedRadish <bitedradish@imuhyeon-ui-MacBookAir.local>
2024-02-07 17:56:30 +0900

    commit

--- boj/forEachPractice.js
+++
@@ -1,9 +0,0 @@
-const arr = [1, 2, 3, 4];
-
-arr.forEach((el, idx, arr) => {
-    console.log(`element : ${el} , idx : ${idx} , arr : ${arr}`);
-});
-
-const newArr = arr.map((cur, idx) => {
-    console.log(`cur : ${cur} idx : ${idx} `);
-});
